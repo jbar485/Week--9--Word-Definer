987ca3880b6e4a6617cf0fd31acc163daf1db72c
Judah Barton <judah.barton@yahoo.com>
2019-12-20 15:13:48 -0800

    add html and integration tests

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -15,12 +15,14 @@ describe("create a word path", {:type => :feature}) do
 end
 
 
-describe("create a definition path", {:type => :feature}) do
-  it "creates a word and then goes to the word itself" do
+describe('create a definition path', {:type => :feature}) do
+  it('creates an word and then goes to the word page') do
     word = Word.new("Love", nil)
-    word.save()
+    word.save
     visit("/words/#{word.id}")
     save_and_open_page
-    fill_in("")
+    fill_in('definition_name', :with => 'Act of loving someone')
+    click_on('Add')
+    expect(page).to have_content('Act of loving someone')
   end
 end
