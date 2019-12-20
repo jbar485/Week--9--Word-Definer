4a944b8a1fe571347865c264ff921a9cbf673699
Judah Barton <judah.barton@yahoo.com>
2019-12-20 15:42:46 -0800

    add a fully functioning code

--- app.rb
+++ app.rb
@@ -54,3 +54,29 @@ delete("/words/:id") do
   @words = Word.all
   erb(:words)
 end
+
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
