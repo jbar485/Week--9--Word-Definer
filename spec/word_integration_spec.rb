27e8dde93f0158a02f9ef39ab11a7bdb666b316b
Judah Barton <judah.barton@yahoo.com>
2019-12-20 14:32:44 -0800

    add some of my integration tests

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,25 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe("create a word path", {:type => :feature}) do
+  it "creates a word and then navigates to the word page" do
+    visit("/words")
+    save_and_open_page
+    click_on("Add a New Word")
+    fill_in("word_name", :with => "Awesome")
+    click_on("Create")
+    expect(page).to have_content("Awesome")
+  end
+end
+
+
+describe("create a definition path", {:type => :feature}) do
+  it "creates a word and then goes to the word itself" do
+    word = Word.new("Love", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    save_and_open_page
+    fill_in("")
+  end
+end
