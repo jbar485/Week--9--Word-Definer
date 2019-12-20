b72623c74f8c5365fbfda433b7e88493bb65481b
Judah Barton <judah.barton@yahoo.com>
2019-12-20 10:44:35 -0800

    add more things to app.rb as well as html

--- app.rb
+++ app.rb
@@ -18,3 +18,25 @@ get("/words") do
   end
   erb(:words)
 end
+
+get("/words/new") do
+  erb(:new_word)
+end
+
+post("/words") do
+  name = params[:word_name]
+  word = Word.new(name,  nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get("/words/:id") do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get("/words/:id/edit") do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
